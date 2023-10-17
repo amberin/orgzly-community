d24d6eacdbc38f594c213ccb82b2f8bf382d629d
Victor Andreasson <victor.andreasson@fripost.org>
2023-10-17 23:22:23 +0200

    wip

--- app/src/main/java/com/orgzly/android/usecase/RepoUpdate.kt
+++ app/src/main/java/com/orgzly/android/usecase/RepoUpdate.kt
@@ -5,8 +5,7 @@ import com.orgzly.android.repos.RepoWithProps
 
 class RepoUpdate(val repoWithProps: RepoWithProps) : UseCase() {
     override fun run(dataRepository: DataRepository): UseCaseResult {
-        val id = dataRepository.updateRepo(repoWithProps)
-
-        return UseCaseResult(id)
+        dataRepository.updateRepo(repoWithProps)
+        return UseCaseResult(repoWithProps.repo.id)
     }
 }
