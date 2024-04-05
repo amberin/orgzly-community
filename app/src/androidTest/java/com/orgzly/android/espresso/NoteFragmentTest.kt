0c5321f47f2ff4c54c377fdf6a211e7c179dd6dc
Victor Andreasson <amberin@users.noreply.github.com>
2024-04-05 12:26:29 +0200

    Sleep

--- app/src/androidTest/java/com/orgzly/android/espresso/NoteFragmentTest.kt
+++ app/src/androidTest/java/com/orgzly/android/espresso/NoteFragmentTest.kt
@@ -523,7 +523,7 @@ class NoteFragmentTest : OrgzlyTest() {
         onNoteInBook(1).perform(longClick())
         onActionItemClick(R.id.new_note, R.string.new_note);
         onView(withText(R.string.new_under)).perform(click())
-        SystemClock.sleep(500)
+        SystemClock.sleep(1000)
         onView(withId(R.id.title_edit)).perform(*replaceTextCloseKeyboard("1.1"))
         onView(withId(R.id.breadcrumbs_text)).perform(clickClickableSpan("Note #1."))
 
@@ -535,7 +535,7 @@ class NoteFragmentTest : OrgzlyTest() {
         onView(withText(R.string.cancel)).perform(click())
 
         // Title remains the same
-        SystemClock.sleep(500)
+        SystemClock.sleep(1000)
         onView(withId(R.id.title_edit)).check(matches(withText("1.1")))
     }
 
