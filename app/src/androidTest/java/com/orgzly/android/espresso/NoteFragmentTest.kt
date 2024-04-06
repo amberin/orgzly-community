b0c4ae1d633a939f188daecc8e0330037e28ae7e
Victor Andreasson <victor.andreasson@fripost.org>
2024-04-06 23:19:52 +0200

    Sleep

--- app/src/androidTest/java/com/orgzly/android/espresso/NoteFragmentTest.kt
+++ app/src/androidTest/java/com/orgzly/android/espresso/NoteFragmentTest.kt
@@ -532,7 +532,7 @@ class NoteFragmentTest : OrgzlyTest() {
         onView(withText(R.string.cancel)).perform(click())
 
         // Title remains the same
-        SystemClock.sleep(2500)
+        SystemClock.sleep(3000)
         onView(withId(R.id.title_edit)).check(matches(withText("1.1")))
     }
 
