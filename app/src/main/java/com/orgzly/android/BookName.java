f546a050fd0a54f29a1ab3e5a152b4b925b8dd24
Victor Andreasson <victor.andreasson@fripost.org>
2024-07-05 22:54:28 +0200

    Handle non-"content" repo file URLs better

--- app/src/main/java/com/orgzly/android/BookName.java
+++ app/src/main/java/com/orgzly/android/BookName.java
@@ -63,12 +63,17 @@ public class BookName {
     }
 
     public static String getFileName(Uri repoUri, Uri fileUri) {
+        /* The content:// repository type requires special handling */
         if ("content".equals(repoUri.getScheme())) {
             String repoUriLastSegment = repoUri.toString().replaceAll("^.*/", "");
             String repoRootUriSegment = repoUri + "/document/" + repoUriLastSegment + "%2F";
             return Uri.decode(fileUri.toString().replace(repoRootUriSegment, ""));
         } else {
-            return fileUri.toString().replace(repoUri.toString(), "");
+            // Just return the decoded fileUri stripped of the repoUri (if present), and stripped
+            // of any leading / (if present).
+            return Uri.decode(
+                    fileUri.toString().replace(repoUri.toString(), "")
+            ).replaceFirst("^/", "");
         }
     }
 
